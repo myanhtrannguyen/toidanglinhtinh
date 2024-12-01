9173128db0ac7afafb03dce718df19ac5f423ea4
Trần Nguyễn Mỹ Anh <myanh260305@gmail.com>
2024-12-01 11:08:06 +0700

    Tao tram cam

--- UI_UX/scripts.js
+++
@@ -1,8 +0,0 @@
-function nextPage() {
-    const currentUrl = window.location.href;
-    if (currentUrl.includes("page1")) {
-        window.location.href = "page2.html";
-    } else if (currentUrl.includes("page2")) {
-        window.location.href = "page3.html";
-    }
-}
